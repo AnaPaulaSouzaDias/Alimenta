c4501e56237eaca735eef04dd8d08ade1f90feb7
HenriqueUriel <riikilopes262@hotmail.com>
2021-10-07 13:34:06 -0300

    atualizações para subir no deploy

--- Alimenta/src/main/java/com/Alimenta/Alimenta/controller/UsuarioController.java
+++ Alimenta/src/main/java/com/Alimenta/Alimenta/controller/UsuarioController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Alimenta.Alimenta.model.UserLogin;
 import com.Alimenta.Alimenta.model.Usuario;
+import com.Alimenta.Alimenta.repository.UsuarioRepository;
 import com.Alimenta.Alimenta.service.UsuarioService;
 
 @RestController
@@ -24,11 +27,21 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@GetMapping("/all")
 	public ResponseEntity <List<Usuario>> getAll(){	
 		return ResponseEntity.ok(usuarioService.listarUsuarios());
 		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return usuarioRepository.findById(id)
+			.map(resp -> ResponseEntity.ok(resp))
+			.orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping("/logar")
@@ -53,4 +66,8 @@ public class UsuarioController {
 			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 
+	@DeleteMapping("/{id}")
+	public void deleteUsuario (@PathVariable long id) {
+		usuarioRepository.deleteById(id);
+	}
 }
